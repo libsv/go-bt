048d6feb0bd672d5a5651907513194d8db1f7009
David Watson <d.watson@nchain.com>
2019-11-27 11:02:18 +0000

    Remove old code

--- AddressFromPublicKey.go
+++ AddressFromPublicKey.go
@@ -1,29 +1,14 @@
 package cryptolib
 
 import (
-	"crypto/sha256"
+	"fmt"
 
 	"github.com/btcsuite/btcd/btcec"
-	"github.com/btcsuite/btcutil/base58"
 )
-
-func base58Encode(input []byte) string {
-	b := make([]byte, 0, len(input)+4)
-	b = append(b, input[:]...)
-	cksum := checksum(b)
-	b = append(b, cksum[:]...)
-	return base58.Encode(b)
-}
-
-func checksum(input []byte) (cksum [4]byte) {
-	h := sha256.Sum256(input)
-	h2 := sha256.Sum256(h[:])
-	copy(cksum[:], h2[:4])
-	return
-}
 
 // AddressFromPublicKeyHash comment
 func AddressFromPublicKeyHash(pubKey *btcec.PublicKey, mainnet bool) string {
+	fmt.Println("AddressFromPublicKeyHash() is deprecated - please use NewAddressFromPublicKeyHash() instead")
 	hash := Hash160(pubKey.SerializeCompressed())
 
 	// regtest := 111
