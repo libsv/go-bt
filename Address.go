5dab5805fc0e30a8e4ce1a627b5a03f6f12e481a
David Watson <d.watson@nchain.com>
2019-11-27 10:28:09 +0000

    Add address funcs

--- Address.go
+++ Address.go
@@ -1,12 +1,48 @@
 package cryptolib
 
 import (
+	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+
+	"github.com/btcsuite/btcutil/base58"
 )
 
-// AddressToPubKeyHash comment
-func AddressToPubKeyHash(address string) (publicKeyHash string, err error) {
+// Address comment
+type Address struct {
+	AddressString string
+	PublicKeyHash string
+}
+
+// NewAddressFromString comment
+func NewAddressFromString(addr string) (*Address, error) {
+	pkh, err := pubKeyHash(addr)
+	if err != nil {
+		return nil, err
+	}
+	a := Address{
+		AddressString: addr,
+		PublicKeyHash: pkh,
+	}
+	return &a, nil
+}
+
+// NewAddressFromPublicKey comment
+func NewAddressFromPublicKey(pubKey string, mainnet bool) (*Address, error) {
+	return NewAddressFromPublicKeyHash(Hash160([]byte(pubKey)), mainnet)
+}
+
+// NewAddressFromPublicKeyHash comment
+func NewAddressFromPublicKeyHash(hash []byte, mainnet bool) (*Address, error) {
+	a := Address{
+		AddressString: addressFromPublicKeyHash(hash, mainnet),
+		PublicKeyHash: hex.EncodeToString(hash),
+	}
+	return &a, nil
+}
+
+// pubKeyHash comment
+func pubKeyHash(address string) (publicKeyHash string, err error) {
 	decoded, err := DecodeString(address)
 
 	if err != nil {
@@ -32,4 +68,34 @@ func AddressToPubKeyHash(address string) (publicKeyHash string, err error) {
 	default:
 		return "", fmt.Errorf("Address %s is not supported", address)
 	}
+}
+
+func base58Encode(input []byte) string {
+	b := make([]byte, 0, len(input)+4)
+	b = append(b, input[:]...)
+	cksum := checksum(b)
+	b = append(b, cksum[:]...)
+	return base58.Encode(b)
+}
+
+func checksum(input []byte) (cksum [4]byte) {
+	h := sha256.Sum256(input)
+	h2 := sha256.Sum256(h[:])
+	copy(cksum[:], h2[:4])
+	return
+}
+
+// AddressFromPublicKeyHash comment
+func addressFromPublicKeyHash(hash []byte, mainnet bool) string {
+
+	// regtest := 111
+	// mainnet: 0
+
+	bb := make([]byte, 1)
+	if mainnet == false {
+		bb[0] = 111
+	}
+
+	bb = append(bb, hash...)
+	return base58Encode(bb)
 }
--- Address_test.go
+++ Address_test.go
@@ -1,15 +1,23 @@
 package cryptolib
 
-import "testing"
+import (
+	"encoding/hex"
+	"fmt"
+	"testing"
+
+	"github.com/btcsuite/btcutil"
+)
 
 func TestAddressToPubKeyHash(t *testing.T) {
 	publicKeyhash := "8fe80c75c9560e8b56ed64ea3c26e18d2c52211b"
 
 	addressTestnet := "mtdruWYVEV1wz5yL7GvpBj4MgifCB7yhPd"
-	expectedPublicKeyhashTestnet, err := AddressToPubKeyHash(addressTestnet)
+	addr, err := NewAddressFromString(addressTestnet)
+	expectedPublicKeyhashTestnet := addr.PublicKeyHash
 
 	addressLive := "1E7ucTTWRTahCyViPhxSMor2pj4VGQdFMr"
-	expectedPublicKeyhashLivenet, err := AddressToPubKeyHash(addressLive)
+	addr2, err := NewAddressFromString(addressLive)
+	expectedPublicKeyhashLivenet := addr2.PublicKeyHash
 
 	if err != nil {
 		t.Error(err)
@@ -21,5 +29,39 @@ func TestAddressToPubKeyHash(t *testing.T) {
 
 	if publicKeyhash != expectedPublicKeyhashLivenet {
 		t.Errorf("PKH from Live address %s incorrect,\ngot: %s\nexpected: %s", addressLive, publicKeyhash, expectedPublicKeyhashTestnet)
+	}
+}
+
+func TestPublicKeyToAddress(t *testing.T) {
+	publicKey := "0285e9737a74c30a873f74df05124f2aa6f53042c2fc0a130d6cbd7d16b944b004"
+	// pkh := "9cf8b938ce2b14f68c59d7ed166b2ae242198037"
+
+	wif, err := btcutil.DecodeWIF("cNGwGSc7KRrTmdLUZ54fiSXWbhLNDc2Eg5zNucgQxyQCzuQ5YRDq")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(hex.EncodeToString(wif.PrivKey.PubKey().SerializeUncompressed()))
+	fmt.Println(AddressFromPublicKeyHash(wif.PrivKey.PubKey(), false))
+	t.Fail()
+
+	expectedAddressTestnet := "mupwfbLpEposb7h4E8WyxCWbt5UYMHcV27"
+	addr, err := NewAddressFromPublicKey(publicKey, false)
+	addressTestnet := addr.AddressString
+
+	expectedAddressLive := "1FJzNYFqRoNcp1DSWZYc8HJH25sqPgmyw3"
+	addr2, err := NewAddressFromPublicKey(publicKey, true)
+	addressLive := addr2.AddressString
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if addressTestnet != expectedAddressTestnet {
+		t.Errorf("Address Testnet from public key address %s incorrect,\ngot: %s\nexpected: %s", publicKey, addressTestnet, expectedAddressTestnet)
+	}
+
+	if addressLive != expectedAddressLive {
+		t.Errorf("Address Live from public key  %s incorrect,\ngot: %s\nexpected: %s", publicKey, addressLive, expectedAddressLive)
 	}
 }
