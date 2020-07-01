2a9e35757d2a00214c35fefffa98d5c2c2e52bce
Jad Wahab <j.wahab@nchain.com>
2020-07-01 16:36:18 +0100

    [add] insternal signer test

--- transaction/Transaction_test.go
+++ transaction/Transaction_test.go
@@ -1,16 +1,12 @@
 package transaction_test
 
 import (
-	"encoding/hex"
 	"reflect"
 	"testing"
 
 	"github.com/libsv/libsv/transaction"
 	"github.com/libsv/libsv/transaction/input"
 	"github.com/libsv/libsv/transaction/output"
-	"github.com/libsv/libsv/transaction/signature"
-
-	"github.com/bitcoinsv/bsvutil"
 
 	"github.com/libsv/libsv/script"
 )
@@ -177,121 +173,6 @@ func TestIsCoinbase(t *testing.T) {
 	if cb2 == true {
 		t.Errorf("Expecting false, got %t", cb2)
 	}
-}
-
-// func TestGetSighashPayload(t *testing.T) {
-// 	unsignedTx := "01000000017e419b1b2dc7d7988bf2c982878d7719bee096d31111a72d1c7470e5ab7d1a5b0000000000ffffffff02404b4c00000000001976a91404ff367be719efa79d76e4416ffb072cd53b208888acde47e976000000001976a9148fe80c75c9560e8b56ed64ea3c26e18d2c52211b88ac00000000"
-// 	tx, err := transaction.NewFromString(unsignedTx)
-// 	// Previous txid 5b1a7dabe570741c2da71111d396e0be19778d8782c9f28b98d7c72d1b9b417e
-// 	if err != nil {
-// 		t.Fatal("Failed to create transaction")
-// 	}
-//
-// 	// Add the UTXO amount and script.
-//
-// 	firstInput := tx.Inputs[0]
-// 	firstInput.PreviousTxSatoshis = 2000000000
-// 	firstInput.PreviousTxScript, _ = script.NewFromHexString("76a9148fe80c75c9560e8b56ed64ea3c26e18d2c52211b88ac")
-// 	// t.Logf("%x\n", tx.ToBytes())
-// 	// tx with input 01000000017e419b1b2dc7d7988bf2c982878d7719bee096d31111a72d1c7470e5ab7d1a5b000000001976a9148fe80c75c9560e8b56ed64ea3c26e18d2c52211b88acffffffff02404b4c00000000001976a91404ff367be719efa79d76e4416ffb072cd53b208888acde47e976000000001976a9148fe80c75c9560e8b56ed64ea3c26e18d2c52211b88ac00000000
-//
-// 	sigType := uint32(transaction.SighashAll | transaction.SighashForkID)
-// 	sigHashes, err := tx.GetSighashPayload(sigType)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-//
-// 	if len(*sigHashes) != 1 {
-// 		t.Errorf("Error expected payload to be 1 item long, got %d", len(*sigHashes))
-// 	}
-//
-// 	expectedPayload := transaction.NewSigningPayload()
-// 	// Add the expected payload for the single input.
-// 	publicKeyhash := "8fe80c75c9560e8b56ed64ea3c26e18d2c52211b" // This is the PKH for address mtdruWYVEV1wz5yL7GvpBj4MgifCB7yhPd
-// 	expectedPayload.AddItem(publicKeyhash, "8ea09cb667b276a886b79d8d6b7d073cc88e64f1640dc9bfd400f9301d4aaa98")
-//
-// 	if !reflect.DeepEqual(*sigHashes, *expectedPayload) {
-// 		t.Errorf("Error expected payload does not match actual, \n      got %+v\n, \nexpected, %+v\n", (*sigHashes)[0], (*expectedPayload)[0])
-// 	}
-//
-// 	if err != nil {
-// 		t.Error(err)
-// 		return
-// 	}
-// }
-
-// func TestApplySignatures(t *testing.T) {
-//
-// 	unsignedTx := "010000000236916d2d420bbd4ff8cd94a2b49d89daeeaeeedbf640cd2c9aa0c619bd806209000000001976a914bcd0bdbf5fcde5ed957396752d4bd2e01d36870288acffffffff3fdb6bf215bad39941525500337e9e7924f99da5a841c5dc7c1eab8036162fe2000000001976a914bcd0bdbf5fcde5ed957396752d4bd2e01d36870288acffffffff0380d1f008000000001976a91490d7b4c4df77b035616e53e2f3701ab562d6f87f88ac80f0fa02000000001976a91490e5bc4b4b5391b60c3fa9b568f916fa83819fce88ac000000000000000020006a1d536f6d652064617461203132333435363738383930206162636465666700000000"
-// 	tx, err := transaction.NewFromString(unsignedTx)
-// 	if err != nil {
-// 		t.Error(err)
-// 		return
-// 	}
-//
-// 	signingPayload := transaction.SigningPayload{}
-//
-// 	// Append a valid response received from the signing service for this Tx.
-// 	signingItem := transaction.SigningItem{
-// 		PublicKeyHash: "bcd0bdbf5fcde5ed957396752d4bd2e01d368702",
-// 		SigHash:       "80448cea404b51f82d409cbd1fbca66bf43fe1cd45d7660953e39ce3c5d8208d",
-// 		PublicKey:     "02ba6bc6906e4937bcde60dbbabdd994dbd0c23e86d834a856091efe677be378b1",
-// 		Signature:     "3045022100a0a005f339978dd6945e44d524d576189f8f7546f41c4899beaa796facb0c4c40220719de9a73796d604b9ee32d7496234c488705fa73f0bd2ffeadcca57580f4cb3",
-// 	}
-// 	signingPayload = append(signingPayload, &signingItem)
-//
-// 	signingItem2 := transaction.SigningItem{
-// 		PublicKeyHash: "bcd0bdbf5fcde5ed957396752d4bd2e01d368702",
-// 		SigHash:       "c62573ac749d9b202cd7b2e0d36a0f688a680810a70ee840f6de7bab4d615095",
-// 		PublicKey:     "02ba6bc6906e4937bcde60dbbabdd994dbd0c23e86d834a856091efe677be378b1",
-// 		Signature:     "30440220399173272f0f56c06b4eb1ccce970603e305988788ab1468e0948ae340fc5380022067684423502f75c5b6e88ad302cc2a1cf739c824efbd5e83fa9e02d4b2975f64",
-// 	}
-// 	signingPayload = append(signingPayload, &signingItem2)
-//
-// 	err = tx.ApplySignatures(&signingPayload, 0)
-// 	if err != nil {
-// 		t.Error(err)
-// 		return
-// 	}
-//
-// 	signedTxFromRegtest := "010000000236916d2d420bbd4ff8cd94a2b49d89daeeaeeedbf640cd2c9aa0c619bd806209000000006b483045022100a0a005f339978dd6945e44d524d576189f8f7546f41c4899beaa796facb0c4c40220719de9a73796d604b9ee32d7496234c488705fa73f0bd2ffeadcca57580f4cb3412102ba6bc6906e4937bcde60dbbabdd994dbd0c23e86d834a856091efe677be378b1ffffffff3fdb6bf215bad39941525500337e9e7924f99da5a841c5dc7c1eab8036162fe2000000006a4730440220399173272f0f56c06b4eb1ccce970603e305988788ab1468e0948ae340fc5380022067684423502f75c5b6e88ad302cc2a1cf739c824efbd5e83fa9e02d4b2975f64412102ba6bc6906e4937bcde60dbbabdd994dbd0c23e86d834a856091efe677be378b1ffffffff0380d1f008000000001976a91490d7b4c4df77b035616e53e2f3701ab562d6f87f88ac80f0fa02000000001976a91490e5bc4b4b5391b60c3fa9b568f916fa83819fce88ac000000000000000020006a1d536f6d652064617461203132333435363738383930206162636465666700000000"
-//
-// 	if hex.EncodeToString(tx.ToBytes()) != signedTxFromRegtest {
-// 		t.Errorf("Error - tx with sigs applied does not match expcted signed tx from regtest.\nGot %s\nexpected %s\n", hex.EncodeToString(tx.ToBytes()), signedTxFromRegtest)
-// 	}
-// }
-
-// We don't expect to use the local tx.sign() function as the signing service does it, but we include it for completeness using the same methods.
-func TestSignTx(t *testing.T) {
-	unsignedTx := "010000000193a35408b6068499e0d5abd799d3e827d9bfe70c9b75ebe209c91d25072326510000000000ffffffff02404b4c00000000001976a91404ff367be719efa79d76e4416ffb072cd53b208888acde94a905000000001976a91404d03f746652cfcb6cb55119ab473a045137d26588ac00000000"
-	tx, err := transaction.NewFromString(unsignedTx)
-
-	if err != nil {
-		t.Fatal("Failed to create transaction")
-	}
-	// Add the UTXO amount and script.
-	tx.Inputs[0].PreviousTxSatoshis = 100000000
-	tx.Inputs[0].PreviousTxScript, _ = script.NewFromHexString("76a914c0a3c167a28cabb9fbb495affa0761e6e74ac60d88ac")
-
-	// Our private key.
-	wif, err := bsvutil.DecodeWIF("cNGwGSc7KRrTmdLUZ54fiSXWbhLNDc2Eg5zNucgQxyQCzuQ5YRDq")
-	if err != nil {
-		t.Fatal(err)
-	}
-	signer := signature.InternalSigner{PrivateKey: wif.PrivKey, SigHashFlag: 0}
-	err = tx.SignAuto(&signer)
-	if err != nil {
-		t.Fatal(err)
-	}
-	expectedSignedTx := "010000000193a35408b6068499e0d5abd799d3e827d9bfe70c9b75ebe209c91d2507232651000000006b483045022100c1d77036dc6cd1f3fa1214b0688391ab7f7a16cd31ea4e5a1f7a415ef167df820220751aced6d24649fa235132f1e6969e163b9400f80043a72879237dab4a1190ad412103b8b40a84123121d260f5c109bc5a46ec819c2e4002e5ba08638783bfb4e01435ffffffff02404b4c00000000001976a91404ff367be719efa79d76e4416ffb072cd53b208888acde94a905000000001976a91404d03f746652cfcb6cb55119ab473a045137d26588ac00000000"
-
-	if hex.EncodeToString(tx.ToBytes()) != expectedSignedTx {
-		t.Errorf("Expecting %s\n, got %s\n", expectedSignedTx, hex.EncodeToString(tx.ToBytes()))
-	}
-	//
-	//if unsignedTx == expectedSignedTx {
-	//	t.Errorf("Expected and signed TX strings in code identical")
-	//}
 }
 
 // func TestValidSignature(t *testing.T) {
---
+++ transaction/signature/InternalSigner_test.go
@@ -0,0 +1,43 @@
+package signature_test
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/bitcoinsv/bsvutil"
+	"github.com/libsv/libsv/script"
+	"github.com/libsv/libsv/transaction"
+	"github.com/libsv/libsv/transaction/signature"
+)
+
+func TestSignAuto(t *testing.T) {
+	unsignedTx := "010000000193a35408b6068499e0d5abd799d3e827d9bfe70c9b75ebe209c91d25072326510000000000ffffffff02404b4c00000000001976a91404ff367be719efa79d76e4416ffb072cd53b208888acde94a905000000001976a91404d03f746652cfcb6cb55119ab473a045137d26588ac00000000"
+	tx, err := transaction.NewFromString(unsignedTx)
+
+	if err != nil {
+		t.Fatal("Failed to create transaction")
+	}
+	// Add the UTXO amount and script.
+	tx.Inputs[0].PreviousTxSatoshis = 100000000
+	tx.Inputs[0].PreviousTxScript, _ = script.NewFromHexString("76a914c0a3c167a28cabb9fbb495affa0761e6e74ac60d88ac")
+
+	// Our private key.
+	wif, err := bsvutil.DecodeWIF("cNGwGSc7KRrTmdLUZ54fiSXWbhLNDc2Eg5zNucgQxyQCzuQ5YRDq")
+	if err != nil {
+		t.Fatal(err)
+	}
+	signer := signature.InternalSigner{PrivateKey: wif.PrivKey, SigHashFlag: 0}
+	err = tx.SignAuto(&signer)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectedSignedTx := "010000000193a35408b6068499e0d5abd799d3e827d9bfe70c9b75ebe209c91d2507232651000000006b483045022100c1d77036dc6cd1f3fa1214b0688391ab7f7a16cd31ea4e5a1f7a415ef167df820220751aced6d24649fa235132f1e6969e163b9400f80043a72879237dab4a1190ad412103b8b40a84123121d260f5c109bc5a46ec819c2e4002e5ba08638783bfb4e01435ffffffff02404b4c00000000001976a91404ff367be719efa79d76e4416ffb072cd53b208888acde94a905000000001976a91404d03f746652cfcb6cb55119ab473a045137d26588ac00000000"
+
+	if hex.EncodeToString(tx.ToBytes()) != expectedSignedTx {
+		t.Errorf("Expecting %s\n, got %s\n", expectedSignedTx, hex.EncodeToString(tx.ToBytes()))
+	}
+	//
+	//if unsignedTx == expectedSignedTx {
+	//	t.Errorf("Expected and signed TX strings in code identical")
+	//}
+}
