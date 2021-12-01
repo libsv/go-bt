9875c39335d7ee4707d596c17eb013cfd722df2b
Tíghearnán Carroll <t.carroll@nchain.com>
2021-12-01 18:46:11 +0000

    unexported test data dir

--- testing/data/data.go
+++ testing/data/data.go
@@ -6,8 +6,8 @@ import (
 	"path"
 )
 
-// TestDataDir a directory container test data.
-type TestDataDir struct {
+// testDataDir a directory container test data.
+type testDataDir struct {
 	prefix string
 	fs     embed.FS
 }
@@ -16,17 +16,17 @@ type TestDataDir struct {
 var txBinData embed.FS
 
 // TxBinData data for binary txs.
-var TxBinData = TestDataDir{
+var TxBinData = testDataDir{
 	prefix: "tx/bin",
 	fs:     txBinData,
 }
 
 // Open a file.
-func (d *TestDataDir) Open(file string) (fs.File, error) {
+func (d *testDataDir) Open(file string) (fs.File, error) {
 	return d.fs.Open(path.Join(d.prefix, file))
 }
 
 // Load the data of a file.
-func (d *TestDataDir) Load(file string) ([]byte, error) {
+func (d *testDataDir) Load(file string) ([]byte, error) {
 	return d.fs.ReadFile(path.Join(d.prefix, file))
 }
